855c33023b48dd72aff97956c454830e120f5750
Sönke Kluth <mail@soenkekluth.com>
2017-04-16 06:59:34 +0200

    add static resolve and res as alias

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const prs = resolver => new Promise(resolver);
+prs.resolve = prs.res = Promise.resolve.bind(Promise);
+prs.all = Promise.all.bind(Promise);
 module.exports = prs;
--- test.js
+++ test.js
@@ -2,12 +2,27 @@ import test from 'ava';
 import prs from './index';
 
 const resolver = (resolve, reject) => {
-  setTimeout(()=>{
+  setTimeout(() => {
     resolve('check');
   }, 100);
 }
 
-test('promise resolves after 100ms', async t => {
+test('prs(resolver) resolves after 100ms', async t => {
   const value = await prs(resolver);
   t.is(value, 'check');
+});
+
+test('prs.resolve (static)', async t => {
+  const value = await prs.resolve('check');
+  t.is(value, 'check');
+});
+
+test('prs.res (static)', async t => {
+  const value = await prs.res('check');
+  t.is(value, 'check');
+});
+
+test('prs.all', async t => {
+  const values = await prs.all([prs(resolver), prs(resolver), prs(resolver), prs(resolver), prs(resolver)]);
+  t.deepEqual(values, ['check', 'check', 'check', 'check', 'check']);
 });
