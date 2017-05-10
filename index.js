7de05b406c3a01b7a110bc82b388025a8b17b14d
Sönke Kluth <mail@soenkekluth.com>
2017-05-10 16:02:46 +0200

    switch from es6 to es5

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const prs = resolver => new Promise(resolver);
+var prs = function(resolver){
+	return new Promise(resolver);
+};
 prs.resolve = prs.res = Promise.resolve.bind(Promise);
+prs.reject = prs.rej = Promise.reject.bind(Promise);
 prs.all = Promise.all.bind(Promise);
 module.exports = prs;
